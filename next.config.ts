60d7af73481639009323bd13e963cae0a508f51f
ClaudioLitz <claudio_litz@estudante.sesisenai.org.br>
2026-04-08 08:28:23 -0300

    fix: downgrade next to 15.1.12

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
